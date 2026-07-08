bd588e37ace5d0beac82eb77cc6ff9ec2fdd4043
Vanshika Bhagal <bhagalvanshika@gmail.com>
2026-07-08 16:35:41 +0530

    Solved: Find Element at a Given Index

---
+++ ARRAYS/01_find_element_at_given_index.py
@@ -0,0 +1,10 @@
+arr = [10, 20, 30, 40, 50]
+index = int(input("Which index value do you want? "))
+
+#for error handling
+if(index>len(arr)):
+    print("Invalid index.")
+#output
+else:
+    print(f"The value of arr[{index}] is {arr[index]}.")
+
