be142d25618f573eb774102ce5c0defc9d21627b
Vanshika Bhagal <bhagalvanshika@gmail.com>
2026-07-10 15:43:43 +0530

    Solved: Alternates in Arrays

---
+++ ARRAYS/05_alternates_in_arrays.py
@@ -0,0 +1,8 @@
+def Alternate(array):
+    result = []
+    for index in range(0, len(array, 2)):
+        result.append(array[index])
+    return result
+
+test = [1,2,3,4,5,6,7]
+print(Alternate(test))
