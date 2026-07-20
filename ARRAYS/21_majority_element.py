e9372eb9d7140cccb30f1da74c5165b6dc95d0ee
Vanshika Bhagal <bhagalvanshika@gmail.com>
2026-07-20 17:18:10 +0530

    Optimized: Majority element in array

--- ARRAYS/21_majority_element.py
+++ ARRAYS/21_majority_element.py
@@ -1,13 +1,31 @@
 class Solution:
     def majorityElement(self, nums):
-        n = len(nums)
-        for number in set(nums):
-            count = 0
-            for element in nums:
-                if element == number:
-                    count += 1
-                    if count > n//2:
-                        return number
+        # Came up by myself #
+        # n = len(nums)
+        # for number in set(nums):
+        #     count = 0
+        #     for element in nums:
+        #         if element == number:
+        #             count += 1
+        #             if count > n//2:
+        #                 return number
+    
+        # Optimised #
+        # n = len(nums)
+        # list.sort(nums)
+        # return nums[n//2]
+
+        # Boyer - Moore Vooting algorithm #
+        freq = 0
+        result = 0
+        for numbers in nums:
+            if freq == 0:
+                result = numbers
+            if result == numbers:
+                freq += 1
+            else:
+                freq -= 1
+        return result
     
 array = [2,2,1,1,1,2,2]
 s1 = Solution()
